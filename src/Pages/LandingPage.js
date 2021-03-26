9336283cef5e312e5dd85f3e46b11e81942d1542
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-03-26 15:23:15 +0700

    MostPicked Styling

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -2,6 +2,8 @@ import Header from 'Parts/Header'
 import React, { Component } from 'react'
 import LandingPageJson from 'json/landingPage.json'
 import Hero from 'Parts/Hero'
+import MostPicked from 'Parts/MostPicked'
+
 
 
 export default class LandingPage extends Component {
@@ -10,6 +12,8 @@ export default class LandingPage extends Component {
       <>
         <Header {...this.props}></Header>
 ;<Hero data={LandingPageJson.hero} />
+;<MostPicked data={LandingPageJson.mostPicked} />
+
 
       </>
     )
