d967e35e211a596eef8dbc9f76fec8bedd5809d4
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-06-23 08:42:49 +0700

    Update index.js

--- src/Elements/Button/index.js
+++ src/Elements/Button/index.js
@@ -39,9 +39,7 @@ if (props.isDisabled) className.push('disabled')
         <a
           href={props.href}
           className={className.join(' ')}
-          style={props.style}
-          target={props.target === '_blank' ? '_blank' : undefined}
-          rel='noreferrer' 
+          style={props.style} 
         >
           {props.children}
         </a>
